19f088ba9b23c7c806b5da5c86097da7c268733e
Nolan <nierardi@radford.edu>
2021-10-16 14:09:46 -0400

    changed to downloading audio

--- simplespeak.py
+++ simplespeak.py
@@ -1,15 +1,29 @@
-import pyttsx3
+from playsound import playsound
+import random
+import gtts
+from time import sleep
 
-def speak_text(text, engine):
-	# Add the text to the engine's queue
-	engine.say(text)
-	# Play the speech
-	engine.runAndWait()
+FILENAMES = [ # files to play - use something like download_audio to download them.
+"livingroomlamp_on.mp3", 
+"livingroomlamp_off.mp3", 
+"weather.mp3",
+"joke.mp3",
+"bedroomlamp_on.mp3",
+"bedroomlamp_off.mp3"
+]
+
+TIME_BETWEEN_REQUESTS = 60 # seconds
+
+def download_audio(text, filename):
+	audio = gtts.gTTS(text)
+	audio.save(filename)
 
 def main():
-	engine = pyttsx3.init()
-	text = "Hey Google, tell me a joke"
-	speak_text(text, engine)
+	while True:
+		filename = random.choice(FILENAMES)
+		playsound(filename)
+		print(f"Played {filename}")
+		sleep(TIME_BETWEEN_REQUESTS)
 
 if __name__ == '__main__':
 	main()
